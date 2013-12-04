9bcfe7ee1c9748bad1560306ce22d2131f4e0a45
kristof <kristof@incubaid.com>
2013-12-04 11:40:36 +0100

    desktop

--- unstable/xfce4desktop/1.0/actions/install.prepare.py
+++ unstable/xfce4desktop/1.0/actions/install.prepare.py
@@ -1,23 +1,20 @@
 def main(j,jp):
    
     #prepare the platform before copying the files
+    j.system.platform.ubuntu.install("aptitude")
 
-    #remove gnome first
-    do.execute("aptitude purge `dpkg --get-selections | grep gnome | cut -f 1` -y")
-    do.execute("aptitude -f install")
-    do.execute("aptitude purge `dpkg --get-selections | grep deinstall | cut -f 1` -y")
-    do.execute("aptitude -f install")
+    # # #remove gnome first
+    # do=j.system.installtools
+    # do.execute("aptitude purge `dpkg --get-selections | grep gnome | cut -f 1` -y")
+    # do.execute("aptitude -f install -y")
+    # do.execute("aptitude purge `dpkg --get-selections | grep deinstall | cut -f 1` -y")
+    # do.execute("aptitude -f install -y")
 
     # can happen by e.g. installing a debian package e.g. by
-    names=["xfce4","scribus","firefox","gimp","tuxcmd","evolution","pidgin","evince","openpro","gnucash","inkscape"]
+    names=["xfce4","scribus","firefox","gimp","tuxcmd","pidgin","evince","gnucash","inkscape","evolution"]
     for packagename in names:
         j.system.platform.ubuntu.install(packagename)
-       
-    #install found debs they need to be in debs dir of one or more of the platforms (all relevant platforms will be used)
-    #args.qp.installUbuntuDebs()
-    
-    #shortcut to some usefull install tools
-    #do=j.system.installtools
+
 
     #configuration is not done in this step !!!!!
     #copying files from files section of jpackages is not done in this step
